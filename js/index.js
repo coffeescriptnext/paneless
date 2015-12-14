132116800a3e046ed19615df851f7c4b87599364
Thomas Broadley <buriedunderbooks@hotmail.com>
2015-12-14 11:47:32 +0900

    added redux-thunk #nowifi

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import root from './reducers';
 import PaneGrid from './components/PaneGrid';
 import Footer from './components/Footer';
 
-const store = createStore(root);
+const store = applyMiddleware(
+  o => next => action =>
+    typeof action === 'function' ?
+      action(o.dispatch, o.getState) :
+      next(action)
+)(createStore)(root);
 
 // Render the top-level class of the application.
 ReactDOM.render(
