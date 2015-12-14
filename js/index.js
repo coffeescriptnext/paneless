8febda6b6df515f9426536a8589dcf4c75c47ca3
Thomas Broadley <buriedunderbooks@hotmail.com>
2015-12-14 18:42:05 +0900

    use redux-thunk package

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import root from './reducers';
 import PaneGrid from './components/PaneGrid';
@@ -10,10 +11,7 @@ import Footer from './components/Footer';
 // Apply our own version of redux-thunk to the store.
 // TODO: replace with the redux-thunk package.
 const store = applyMiddleware(
-  o => next => action =>
-    typeof action === 'function' ?
-      action(o.dispatch, o.getState) :
-      next(action)
+  thunk
 )(createStore)(root);
 
 // Render the top-level class of the application.
